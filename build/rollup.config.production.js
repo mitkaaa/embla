ec4def950a88b8619c2495a408ce180b40fd369f
Dmitry Kuznetsov <Kuznetsov.D.Mikhaylo@sberbank.ru>
2019-01-27 21:36:13 +0300

    Cleanup

--- build/rollup.config.production.js
+++ build/rollup.config.production.js
@@ -1,4 +1,4 @@
-// const { uglify } = require('rollup-plugin-uglify');
+const { uglify } = require('rollup-plugin-uglify');
 const babel = require('rollup-plugin-babel');
 
 const basisConfig = require('./basis');
@@ -10,6 +10,6 @@ module.exports = {
   plugins: [
     ...plugins,
     babel({ exclude: 'node_modules/**' }),
-    // uglify(),
+    uglify(),
   ],
 };
